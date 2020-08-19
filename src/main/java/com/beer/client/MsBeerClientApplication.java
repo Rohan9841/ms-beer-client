961f12ea54faa85a9b2c3ac71b7fb64e5f5ac129
Rohan Maharjan <Rohan9841@gmail.com>
2020-08-19 13:40:56 -0400

    initial commit

---
+++ src/main/java/com/beer/client/MsBeerClientApplication.java
@@ -0,0 +1,13 @@
+package com.beer.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBeerClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beer/client/MsBeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
